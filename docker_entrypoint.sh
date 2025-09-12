3377ce97bb76af24e782a432549704dadcb7d3d5
Paolo Baldini <paolo.baldini.phd@gmail.com>
2025-09-12 12:17:29 +0200

    corrected label in docker entrypoint

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -10,7 +10,7 @@ sed -i "s|../../ca/|$WORK_DIR|" config.json
 sed -i "s|../../fg/|$WORK_DIR|" config.json
 sed -i "s|../../tm/|$WORK_DIR|" config.json
 sed -i "s/\"starting_seed\":        1/\"starting_seed\":        $START_SEED/" config.json
-sed -i "s/\"replicas_count\":       100/\"replicas_count\":       $END_SEED/" config.json
+sed -i "s/\"replicas_count\":       100/\"replicas_count\":       $REPLICAS/" config.json
 
 Xvfb :99 -screen 0 1024x768x16 2>&1 | tee log &
 source .venv/bin/activate
